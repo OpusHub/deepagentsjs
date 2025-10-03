64c9eb07e7af282c923f6211c20a5e971c444b6d
Rhuan Victor <212762553+OpusHub@users.noreply.github.com>
2025-10-03 15:43:44 -0300

    desativando streaming

--- src/model.ts
+++ src/model.ts
@@ -6,19 +6,6 @@ export function getDefaultModel(): LanguageModelLike {
     model: "gemini-2.5-flash",
     temperature: 0.3,
     maxRetries: 3,
-    streaming: true,
-    callbacks: [
-      {
-        handleLLMNewToken(token: string) {
-          process.stdout.write(token);
-        },
-        handleLLMError(err: any) {
-          console.error("Erro no stream Gemini:", err);
-        },
-        handleLLMEnd() {
-          console.log("\n--- Stream finalizado ---");
-        }
-      }
-    ]
+   
   }) as any;
 }
